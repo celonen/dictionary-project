bf6219be21e56ddfd193eda3744084f9029f7216
celonen <84405152+celonen@users.noreply.github.com>
2021-08-20 14:11:43 +0200

    refactored code

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,11 +13,11 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-  }
 
-  // documentation: https://dictionaryapi.dev/e
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-  axios.get(apiUrl).then(handleResponse);
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);

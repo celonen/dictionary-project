09c0d14a65e20322d6fe01ee9f95f2a62e1dea73
celonen <84405152+celonen@users.noreply.github.com>
2021-08-20 14:26:16 +0200

    updated header styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <header className="App-header"></header>
         <main>
           <Dictionary />
         </main>
